e5eb83a18ec2c1f6b9f1fc34ef57cc6f025038ae
Esta Prangel <esta.prangel@gmail.com>
2019-11-03 14:10:24 +0200

    Tokenize text. Count POS.

--- loetu/analyze.py
+++ loetu/analyze.py
@@ -1,3 +1,37 @@
-import preprocessing
+from estnltk import Text
+from collections import Counter
 
-preprocessing.
+
+def tokenize(content):
+
+    text = Text(content)
+
+    items = list(text.get.word_texts.postags.lemmas.as_zip)
+
+    s = []
+    v = []
+    a = []
+    d = []
+    k = []
+
+    for item in items:
+        if item[1] == 'S':
+            s.append(item[2])
+        elif item[1] == 'V':
+            v.append(item[2])
+        elif item[1] == 'A':
+            a.append(item[2])
+        elif item[1] == 'D':
+            d.append(item[2])
+        elif item[1] == 'V':
+            v.append(item[2])
+        else:
+            continue
+
+    all_words = [s, v, a, d, k]
+
+    return all_words
+
+
+def count_words(words):
+    print(Counter(words))
